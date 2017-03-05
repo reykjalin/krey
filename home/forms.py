3f0fd0cdd2faecfa967f3b640bf47100915e1142
Kristófer R <kristoferrt@gmail.com>
2017-03-05 01:44:52 -0500

    Remove junk code from forms.py

--- home/forms.py
+++ home/forms.py
@@ -3,7 +3,3 @@ from django import forms
 class UserForm(forms.Form):
     user_name = forms.CharField(label='User Name', max_length=255, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'User Name'}))
     password = forms.CharField(label='Password', max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
-    def getUser(self):
-        return user_name
-    def getPassword(self):
-        return password
